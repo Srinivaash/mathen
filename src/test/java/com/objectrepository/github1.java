6716a77778abcc449cf1f2f6867771b379e5c7f2
MURUGANANDHAM <nivashtms27@gmail.com>
2021-08-23 19:41:31 +0530

    yes2

---
+++ src/test/java/com/objectrepository/github1.java
@@ -0,0 +1,12 @@
+package com.objectrepository;
+
+public class github1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("gggg");
+System.out.println("ggg1");
+System.out.println("ggg2");
+	}
+
+}
